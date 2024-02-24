16489021a6c062bcad5a6004e8c5c4a4c3951e8c
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-02-24 20:08:42 +0900

    Delete src directory

--- src/HelloJava.java
+++
@@ -1,5 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {    //프로그램의 시작점
-        System.out.println("hello java");
-    }
-}
--- src/variable/Var6.java
+++
@@ -1,8 +0,0 @@
-package variable;
-
-public class Var6 {
-    public static void main(String[] args) {
-        int a;
-        //System.out.println(a); // 초기화하지 않으면 오류
-    }
-}
