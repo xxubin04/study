a2f58f8062ff282a7dd41c0bd9cf2b60b189951e
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-03-24 01:16:48 +0900

    add: 명품 C++ ch.2

---
+++ CPP/Book/명품 C++/Ch.2/Check_p69-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int radius;
+    cin >> radius;
+
+    double area = 3.14 * radius * radius;
+
+    cout << area << endl;
+
+    return 0;
+}
