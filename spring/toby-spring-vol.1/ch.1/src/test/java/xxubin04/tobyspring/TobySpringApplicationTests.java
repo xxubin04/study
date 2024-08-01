96dbdf1709d561132590e6c56eb1408174c69889
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-01 22:47:16 +0900

    Add toby-spring ch.1

---
+++ spring/toby-spring-vol.1/ch.1/src/test/java/xxubin04/tobyspring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package xxubin04.tobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
