e442f91f488f22f1e8a6bb94318e2b0982391ffa
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:25:42 +0900

    Update and rename Exercise3_4.java to Exercise3_04.java

--- Java/Java의 정석/ch03_operator/Exercise3_04.java
+++ Java/Java의 정석/ch03_operator/Exercise3_04.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_4 {
+public class Exercise3_04 {
     public static void main(String[] args) {
         int num = 456;
         System.out.println(num - (num % 100));
