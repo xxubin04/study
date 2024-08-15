afc9fcdee17d7792b49adc9cb63867573ab7ab5b
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:26:42 +0900

    Update and rename Exercise3_9.java to Exercise3_09.java

--- Java/Java의 정석/ch03_operator/Exercise3_09.java
+++ Java/Java의 정석/ch03_operator/Exercise3_09.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_9 {
+public class Exercise3_09 {
     public static void main(String[] args) {
         char ch = 'z';
         boolean b = ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9');
