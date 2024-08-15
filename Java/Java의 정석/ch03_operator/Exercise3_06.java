db2c8bac1fdab4ad738f3fcd86d9fdc5da96edd9
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:26:02 +0900

    Update and rename Exercise3_6.java to Exercise3_06.java

--- Java/Java의 정석/ch03_operator/Exercise3_06.java
+++ Java/Java의 정석/ch03_operator/Exercise3_06.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_6 {
+public class Exercise3_06 {
     public static void main(String[] args) {
         int num = 24;
         System.out.println((num / 10 + 1) * 10 - num);
