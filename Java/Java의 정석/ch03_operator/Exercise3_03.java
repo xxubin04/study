9482f2a6a22c64c8452329bae75f70524f1505d6
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:25:33 +0900

    Update and rename Exercise3_3.java to Exercise3_03.java

--- Java/Java의 정석/ch03_operator/Exercise3_03.java
+++ Java/Java의 정석/ch03_operator/Exercise3_03.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_3 {
+public class Exercise3_03 {
     public static void main(String[] args) {
         int num = 10;
         System.out.println(num != 0 ? (num > 0 ? "양수" : "음수") : 0);
