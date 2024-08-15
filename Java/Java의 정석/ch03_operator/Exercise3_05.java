444cbe181977f536d4a64c306b6ba8e3a9ca75bf
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:25:53 +0900

    Update and rename Exercise3_5.java to Exercise3_05.java

--- Java/Java의 정석/ch03_operator/Exercise3_05.java
+++ Java/Java의 정석/ch03_operator/Exercise3_05.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_5 {
+public class Exercise3_05 {
     public static void main(String[] args) {
         int num = 333;
         System.out.println(num - (num % 10 - 1));
