0be5f3c177e6bd08d042eff0cdc97918af716f15
정유빈 <127945869+xxubin04@users.noreply.github.com>
2024-08-15 15:25:21 +0900

    Update and rename Exercise3_2.java to Exercise3_02.java

--- Java/Java의 정석/ch03_operator/Exercise3_02.java
+++ Java/Java의 정석/ch03_operator/Exercise3_02.java
@@ -1,6 +1,6 @@
 package ch03_operator;
 
-public class Exercise3_2 {
+public class Exercise3_02 {
     public static void main(String[] args) {
         int numOfApples = 123;
         int sizeOfBucket = 10;
